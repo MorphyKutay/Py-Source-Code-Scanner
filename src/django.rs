b2354890c3ac70e1ccec2b012d07ac3f64a66b7d
MorphyKutay <kutaynevzat2006@gmail.com>
2024-09-10 12:35:44 +0300

    save vulnurble code

--- src/django.rs
+++ src/django.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
 use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write, Read, Seek};
 
 pub fn check_risk(contents: &str) -> Vec<(usize, String)> {
     let mut results = Vec::new();
@@ -14,6 +16,24 @@ pub fn check_risk(contents: &str) -> Vec<(usize, String)> {
     results
 }
 
+fn append_to_file(filename: &str, new_content: &str) -> io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(filename)?;
+
+
+    if file.metadata()?.len() > 0 {
+        writeln!(file)?;
+    }
+
+
+    file.write_all(new_content.as_bytes())?;
+    Ok(())
+}
+
+
 pub fn process_file(path: &str) -> std::io::Result<()> {
     let contents = fs::read_to_string(path)?;
     let risky_lines = check_risk(&contents);
@@ -23,6 +43,12 @@ pub fn process_file(path: &str) -> std::io::Result<()> {
         println!("File: {}", path);
         for (line_number, line) in risky_lines {
             println!("Line {}: {}\n-----", line_number, line);
+
+            let filename = "output.txt";
+            let new_content = format!("\n-------\n [!] Potential XSS Vulnerability Found\n Line Number : {}\n Line : {}\n File : {} \n-------\n", line_number, line,path);
+
+            append_to_file(filename, &new_content)?;
+
         }
     }
     
--- src/pathtraversel.rs
+++ src/pathtraversel.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
 use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::{self, Write, Read, Seek};
 
 pub fn check_path_traversal_risk(contents: &str) -> Vec<(usize, String)> {
     let mut results = Vec::new();
@@ -8,15 +10,34 @@ pub fn check_path_traversal_risk(contents: &str) -> Vec<(usize, String)> {
 
     for (line_number, line) in contents.lines().enumerate() {
         if re.is_match(line) {
-            results.push((line_number + 1, line.to_string())); // 1 tabanlı dizin numaralandırma
+            results.push((line_number + 1, line.to_string()));
         }
     }
 
     results
 }
 
+
+fn append_to_file(filename: &str, new_content: &str) -> io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(filename)?;
+
+
+    if file.metadata()?.len() > 0 {
+        writeln!(file)?;
+    }
+
+
+    file.write_all(new_content.as_bytes())?;
+    Ok(())
+}
+
+
 pub fn process_file_for_path_traversal(path: &str) -> std::io::Result<()> {
-    let contents = fs::read_to_string(path)?;  // Dosya içeriğini oku
+    let contents = fs::read_to_string(path)?;
     let risky_lines = check_path_traversal_risk(&contents);
 
     if !risky_lines.is_empty() {
@@ -24,6 +45,12 @@ pub fn process_file_for_path_traversal(path: &str) -> std::io::Result<()> {
         println!("File: {}", path);
         for (line_number, line) in risky_lines {
             println!("Line {}: {}\n-----", line_number, line);
+
+            let filename = "output.txt";
+            let new_content = format!("\n-------\n [!] Potential Path Traversal Vulnerability Found\n Line Number : {}\n Line : {}\n File : {} \n-------\n", line_number, line,path);
+
+            append_to_file(filename, &new_content)?;
+
         }
     }
 
